b1375c7cf2b6595073183600328b7f3f28794465
Freewind <nowindlee@gmail.com>
2019-07-18 16:03:58 +0800

    demo

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
-TypeScript Hello World Demo
-===========================
+TypeScript "for of" with "await" Demo
+======================================
+
+下面这种语法：
+
+```
+for (const x in array) {
+  await doSomething(x);
+} 
+```
+
+对于array中的每一个元素x，会按顺序一个执行完了`doSomething`才会执行另一个。
 
 ```
 npm install
 npm run demo
 ```
-
-Note:
-
-`esModuleInterop` is recommended to set to `true`,
-since we can have consistent importing syntax with babel,
-always use:
-
-```
-import some from 'some'
-```
-
--- hello.ts
+++ hello.ts
@@ -1,1 +1,24 @@
-console.log('Hello');
+async function delay(ms: number): Promise<void> {
+  return new Promise<void>(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  })
+}
+
+const progress: number[] = [];
+
+async function process(num: number) {
+  progress.push(num);
+  await delay(num);
+  progress.push(num + 1);
+}
+
+async function main() {
+  for (const number of [200, 20, 100]) {
+    await process(number);
+  }
+  console.log('progress', progress);
+}
+
+main();
